92a502983e6949711264a6aa32c1c70875d2ca4d
plusisland <plusisland@gmail.com>
2025-02-09 17:07:44 +0800

    Update Post-Pve-Install.sh

--- Post-Pve-Install.sh
+++ Post-Pve-Install.sh
@@ -43,137 +43,104 @@ msg_error() {
 start_routines() {
   header_info
 
-  CHOICE=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "PVE-ENTERPRISE" --menu "The 'pve-enterprise' repository is only available to users who have purchased a Proxmox VE subscription.\n \nDisable 'pve-enterprise' repository?" 14 58 2 \
-    "yes" " " \
-    "no" " " 3>&2 2>&1 1>&3)
-  case $CHOICE in
-  yes)
-    msg_info "Disabling 'pve-enterprise' repository"
-    cat <<EOF >/etc/apt/sources.list.d/pve-enterprise.list
-# deb https://enterprise.proxmox.com/debian/pve bookworm pve-enterprise
-EOF
-    msg_ok "Disabled 'pve-enterprise' repository"
-    ;;
-  no)
-    msg_error "Selected no to Disabling 'pve-enterprise' repository"
-    ;;
-  esac
+CHOICE=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "套件庫設定" --menu "是否設定套件庫：" 10 58 2 \
+    "yes" "是" \
+    "no" "否" 3>&2 2>&1 1>&3)
 
-  CHOICE=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "PVE-NO-SUBSCRIPTION" --menu "The 'pve-no-subscription' repository provides access to all of the open-source components of Proxmox VE.\n \nEnable 'pve-no-subscription' repository?" 14 58 2 \
-    "yes" " " \
-    "no" " " 3>&2 2>&1 1>&3)
-  case $CHOICE in
-  yes)
-    msg_info "Enabling 'pve-no-subscription' repository"
-    cat <<EOF >/etc/apt/sources.list.d/pve-install-repo.list
-deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription
-EOF
-    msg_ok "Enabled 'pve-no-subscription' repository"
-    ;;
-  no)
-    msg_error "Selected no to Enabling 'pve-no-subscription' repository"
-    ;;
-  esac
-
-    CHOICE=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "CEPH PACKAGE REPOSITORIES" --menu "The 'Ceph Package Repositories' provides access to both the 'no-subscription' and 'enterprise' repositories (initially disabled).\n \nCorrect 'ceph package sources?" 14 58 2 \
-      "yes" " " \
-      "no" " " 3>&2 2>&1 1>&3)
-    case $CHOICE in
+case $CHOICE in
     yes)
-      msg_info "Correcting 'ceph package repositories'"
-      cat <<EOF >/etc/apt/sources.list.d/ceph.list
+        # 停用 ceph 企業版套件庫
+        CHOICE_CEPH=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "停用 ceph 企業版套件庫" --menu "是否停用：" 10 58 2 \
+            "yes" "是" \
+            "no" "否" 3>&2 2>&1 1>&3)
+        if [[ "$CHOICE_CEPH" == "yes" ]]; then
+            msg_info "正在停用 ceph 企業版套件庫"
+            cat <<EOF >/etc/apt/sources.list.d/ceph.list
 # deb https://enterprise.proxmox.com/debian/ceph-quincy bookworm enterprise
 EOF
-      msg_ok "Corrected 'ceph package repositories'"
-      ;;
-    no)
-      msg_error "Selected no to Correcting 'ceph package repositories'"
-      ;;
-    esac
+            msg_ok "已停用 ceph 企業版套件庫"
+        fi
+        # 停用企業版軟體庫
+        CHOICE_ENTERPRISE=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "停用 PVE 企業版套件庫" --menu "是否停用：" 10 58 2 \
+            "yes" "是" \
+            "no" "否" 3>&2 2>&1 1>&3)
+        if [[ "$CHOICE_ENTERPRISE" == "yes" ]]; then
+            msg_info "正在停用 PVE 企業版套件庫"
+            cat <<EOF >/etc/apt/sources.list.d/pve-enterprise.list
+# deb https://enterprise.proxmox.com/debian/pve bookworm pve-enterprise
+EOF
+            msg_ok "已停用 PVE 企業版套件庫"
+        fi
+
+        # 啟用非訂閱版軟體庫
+        CHOICE_NO_SUBSCRIPTION=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "啟用 PVE 非訂閱版套件庫" --menu "是否啟用：" 10 58 2 \
+            "yes" "是" \
+            "no" "否" 3>&2 2>&1 1>&3)
+        if [[ "$CHOICE_NO_SUBSCRIPTION" == "yes" ]]; then
+            msg_info "正在啟用 PVE 非訂閱版套件庫"
+            cat <<EOF >/etc/apt/sources.list.d/pve-install-repo.list
+deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription
+EOF
+            msg_ok "已啟用 PVE 非訂閱版套件庫"
+        fi
+        ;;
+esac
 
   if [[ ! -f /etc/apt/apt.conf.d/no-nag-script ]]; then
-    CHOICE=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "SUBSCRIPTION NAG" --menu "This will disable the nag message reminding you to purchase a subscription every time you log in to the web interface.\n \nDisable subscription nag?" 14 58 2 \
-      "yes" " " \
-      "no" " " 3>&2 2>&1 1>&3)
+    CHOICE=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "移除訂閱提示訊息" --menu "是否移除" 14 58 2 \
+      "yes" "是" \
+      "no" "否" 3>&2 2>&1 1>&3)
     case $CHOICE in
     yes)
-      whiptail --backtitle "Proxmox VE Helper Scripts" --msgbox --title "Support Subscriptions" "Supporting the software's development team is essential. Check their official website's Support Subscriptions for pricing. Without their dedicated work, we wouldn't have this exceptional software." 10 58
-      msg_info "Disabling subscription nag"
+      msg_info "移除訂閱提示訊息"
         echo "DPkg::Post-Invoke { \"dpkg -V proxmox-widget-toolkit | grep -q '/proxmoxlib\.js$'; if [ \$? -eq 1 ]; then { echo 'Removing subscription nag from UI...'; sed -i '/.*data\.status.*{/{s/\!//;s/active/NoMoreNagging/}' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js; }; fi\"; };" >/etc/apt/apt.conf.d/no-nag-script
       apt --reinstall install proxmox-widget-toolkit &>/dev/null
-      msg_ok "Disabled subscription nag (Delete browser cache)"
-      ;;
-    no)
-      whiptail --backtitle "Proxmox VE Helper Scripts" --msgbox --title "Support Subscriptions" "Supporting the software's development team is essential. Check their official website's Support Subscriptions for pricing. Without their dedicated work, we wouldn't have this exceptional software." 10 58
-      msg_error "Selected no to Disabling subscription nag"
+      msg_ok "已移除訂閱提示訊息"
       ;;
     esac
   fi
 
-  if ! systemctl is-active --quiet pve-ha-lrm; then
-    CHOICE=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "HIGH AVAILABILITY" --menu "Enable high availability?" 10 58 2 \
-      "yes" " " \
-      "no" " " 3>&2 2>&1 1>&3)
-    case $CHOICE in
+CHOICE=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "高可用性設定" --menu "請選擇高可用性設定：" 14 58 3 \
+    "yes" "啟用高可用性(多節點)" \
+    "no" "停用高可用性(單節點)" 3>&2 2>&1 1>&3)
+
+case $CHOICE in
     yes)
-      msg_info "Enabling high availability"
-      systemctl enable -q --now pve-ha-lrm
-      systemctl enable -q --now pve-ha-crm
-      systemctl enable -q --now corosync
-      msg_ok "Enabled high availability"
-      ;;
+        msg_info "正在啟用高可用性"
+        systemctl enable -q --now pve-ha-lrm
+        systemctl enable -q --now pve-ha-crm
+        systemctl enable -q --now corosync
+        msg_ok "已啟用高可用性"
+        ;;
     no)
-      msg_error "Selected no to Enabling high availability"
-      ;;
-    esac
-  fi
+        msg_info "正在停用高可用性"
+        systemctl disable -q --now pve-ha-lrm
+        systemctl disable -q --now pve-ha-crm
+        systemctl disable -q --now corosync
+        msg_ok "已停用高可用性"
+        ;;
+esac
   
-  if systemctl is-active --quiet pve-ha-lrm; then
-    CHOICE=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "HIGH AVAILABILITY" --menu "If you plan to utilize a single node instead of a clustered environment, you can disable unnecessary high availability (HA) services, thus reclaiming system resources.\n\nIf HA becomes necessary at a later stage, the services can be re-enabled.\n\nDisable high availability?" 18 58 2 \
-      "yes" " " \
-      "no" " " 3>&2 2>&1 1>&3)
-    case $CHOICE in
-    yes)
-      msg_info "Disabling high availability"
-      systemctl disable -q --now pve-ha-lrm
-      systemctl disable -q --now pve-ha-crm
-      systemctl disable -q --now corosync
-      msg_ok "Disabled high availability"
-      ;;
-    no)
-      msg_error "Selected no to Disabling high availability"
-      ;;
-    esac
-  fi
-  
-  CHOICE=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "UPDATE" --menu "\nUpdate Proxmox VE now?" 11 58 2 \
-    "yes" " " \
-    "no" " " 3>&2 2>&1 1>&3)
+  CHOICE=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "更新套件庫" --menu "\n立即更新?" 11 58 2 \
+    "yes" "是" \
+    "no" "否" 3>&2 2>&1 1>&3)
   case $CHOICE in
   yes)
-    msg_info "Updating Proxmox VE (Patience)"
+    msg_info "更新速度受網路速度與硬體等級相關，請耐心等候..."
     apt-get update &>/dev/null
     apt-get -y dist-upgrade &>/dev/null
-    msg_ok "Updated Proxmox VE"
-    ;;
-  no)
-    msg_error "Selected no to Updating Proxmox VE"
+    msg_ok "更新完畢"
     ;;
   esac
 
-  CHOICE=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "REBOOT" --menu "\nReboot Proxmox VE now? (recommended)" 11 58 2 \
-    "yes" " " \
-    "no" " " 3>&2 2>&1 1>&3)
+  CHOICE=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "重新啟動" --menu "\n立刻重啟?" 11 58 2 \
+    "yes" "是" \
+    "no" "否" 3>&2 2>&1 1>&3)
   case $CHOICE in
   yes)
-    msg_info "Rebooting Proxmox VE"
+    msg_info "兩秒後重新啟動"
     sleep 2
-    msg_ok "Completed Post Install Routines"
     reboot
-    ;;
-  no)
-    msg_error "Selected no to Rebooting Proxmox VE (Reboot recommended)"
-    msg_ok "Completed Post Install Routines"
     ;;
   esac
 }
