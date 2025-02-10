ff4cb04131f055bda34200c23d09e1281b3e9486
plusisland <plusisland@gmail.com>
2025-02-10 11:45:31 +0800

    Create hide_no_valid_subscription.sh

---
+++ hide_no_valid_subscription.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# https://forum.snkms.com/post-599
+
+file="/usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js"
+
+show_code='Ext.Msg.show({\s*title: gettext\(\'No valid subscription\'\),\s*icon: Ext.Msg.WARNING,'
+noshow_code='Ext.Msg.noshow({\s*title: gettext\(\\'No valid subscription\\'\),\s*icon: Ext.Msg.WARNING,'
+
+read -p "要隱藏無訂閱訊息視窗嗎？(y/n/r): " choice
+
+case "$choice" in
+  y)
+    sed -i "s/$show_code/$noshow_code/g" "$file"
+    echo "檔案 $file 已修改，重新啟動 pveproxy。"
+    systemctl restart pveproxy
+    ;;
+  n)
+    echo "檔案 $file 未被修改。"
+    ;;
+  r)
+    echo "檔案將還原成官方檔案。" # 新增提示訊息
+    apt-get install --reinstall proxmox-widget-toolkit -y
+    ;;
+  *)
+    echo "無效的選擇。"
+    ;;
+esac
