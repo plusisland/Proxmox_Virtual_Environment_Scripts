041ccc680c7dd91f73ef9fbbdb5ab1edf45bc39b
plusisland <plusisland@gmail.com>
2025-02-11 23:46:08 +0800

    Update install_fake_subscription.sh

--- install_fake_subscription.sh
+++ install_fake_subscription.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 # https://github.com/Jamesits/pve-fake-subscription/tree/master
 
-deb_url="https://github.com/Jamesits/pve-fake-subscription/releases/download/v0.0.11/pve-fake-subscription_0.0.11+git-1_all.deb"
-
 read -p "要安裝 pve-fake-subscription 嗎？(y安裝/n取消/u移除): " choice
 
 case "$choice" in
   y)
-    wget -q https://github.com/Jamesits/pve-fake-subscription/releases/download/v0.0.11/pve-fake-subscription_0.0.11+git-1_all.deb
+    curl -fL "$(curl -fsS https://api.github.com/repos/Jamesits/pve-fake-subscription/releases/latest | sed -r -n 's/.*"browser_download_url": *"(.*\.deb)".*/\1/p')" -O
     dpkg -i pve-fake-subscription_*.deb
     rm -rf pve-fake-subscription_*.deb
     #echo "127.0.0.1 shop.maurer-it.com" | sudo tee -a /etc/hosts
     ;;
   n)
-    echo "檔案 $file 未被修改。"
+    echo "取消安裝。"
     ;;
   u)
+    echo "移除安裝。"
     apt purge pve-fake-subscription
     ;;
 esac
