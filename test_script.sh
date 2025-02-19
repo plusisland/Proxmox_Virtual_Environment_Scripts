3a6f37627eb96c36968946f0f6fcc7eee8c55d9f
plusisland <plusisland@gmail.com>
2025-02-20 00:33:35 +0800

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -32,37 +32,14 @@ else
     echo "parted 已安装"
 fi
 
-if ! command -v expect &> /dev/null
-then
-    echo "expect 未安装，正在安装..."
-    apt install -y expect
-else
-    echo "expect 已安装"
-fi
-
 # 获取未使用的循环设备
 loop_device=$(losetup -f)
-
-# 创建并执行 expect 脚本来自动化 parted 的交互操作
-expect <<EOF
-spawn losetup $loop_device openwrt-*.img
-expect eof
-
-spawn parted \$loop_device print
-expect {
-    -re ".*OK.*" { send "OK\r" }
-    -re ".*Fix.*" { send "Fix\r" }
-}
-
-spawn parted \$loop_device resizepart 2 100%
-expect eof
-
-spawn parted \$loop_device print
-expect eof
-
-spawn losetup -d \$loop_device
-expect eof
-EOF
+# 将映像文件挂载到循环设备
+losetup $loop_device openwrt-*.img
+# 扩展第二个分区到剩余空间
+parted -f -s "$loop_device" resizepart 2 100%
+# 解除挂载循环设备
+losetup -d $loop_device
 
 # 創建虛擬機
 qm create $VMID --name $VMNAME -ostype l26 --machine q35 --bios ovmf --scsihw virtio-scsi-single \
@@ -79,78 +56,63 @@ qm start $VMID
 
 # 發送命令到虛擬機的函數
 function send_line_to_vm() {
-  declare -A keymap
-  keymap=(
-    [" "]="spc"
-    ["-"]="minus"
-    ["="]="equal"
-    [","]="comma"
-    ["."]="dot"
-    ["/"]="slash"
-    ["'"]="apostrophe"
-    [";"]="semicolon"
-    ["\\"]="backslash"
-    ["\`"]="grave_accent"
-    ["["]="bracket_left"
-    ["]"]="bracket_right"
-    ["_"]="shift-minus"
-    ["+"]="shift-equal"
-    ["?"]="shift-slash"
-    ["<"]="shift-comma"
-    [">"]="shift-dot"
-    ['"']="shift-apostrophe"
-    [":"]="shift-semicolon"
-    ["|"]="shift-backslash"
-    ["~"]="shift-grave_accent"
-    ["{"]="shift-bracket_left"
-    ["}"]="shift-bracket_right"
-    ["A"]="shift-a"
-    ["B"]="shift-b"
-    ["C"]="shift-c"
-    ["D"]="shift-d"
-    ["E"]="shift-e"
-    ["F"]="shift-f"
-    ["G"]="shift-g"
-    ["H"]="shift-h"
-    ["I"]="shift-i"
-    ["J"]="shift-j"
-    ["K"]="shift-k"
-    ["L"]="shift-l"
-    ["M"]="shift-m"
-    ["N"]="shift-n"
-    ["O"]="shift-o"
-    ["P"]="shift-p"
-    ["Q"]="shift-q"
-    ["R"]="shift-r"
-    ["S"]="shift-s"
-    ["T"]="shift-t"
-    ["U"]="shift-u"
-    ["V"]="shift-v"
-    ["W"]="shift-w"
-    ["X"]="shift-x"
-    ["Y"]="shift-y"
-    ["Z"]="shift-z"
-    ["!"]="shift-1"
-    ["@"]="shift-2"
-    ["#"]="shift-3"
-    ["$"]="shift-4"
-    ["%"]="shift-5"
-    ["^"]="shift-6"
-    ["&"]="shift-7"
-    ["*"]="shift-8"
-    ["("]="shift-9"
-    [")"]="shift-0"
+  declare -A char_map  # 宣告關聯陣列
+  # 定義字元映射
+  char_map=(
+    [" "] = "spc"
+    ["-"] = "minus"
+    ["="] = "equal"
+    [","] = "comma"
+    ["."] = "dot"
+    ["/"] = "slash"
+    ["'"] = "apostrophe"
+    [";"] = "semicolon"
+    ["\\"] = "backslash"  # 注意反斜線需要跳脫
+    ["\`"] = "grave_accent" # 注意反引號需要跳脫
+    ["["] = "bracket_left"
+    ["]"] = "bracket_right"
+    ["_"] = "shift-minus"
+    ["+"] = "shift-equal"
+    ["?"] = "shift-slash"
+    ["<"] = "shift-comma"
+    [">"] = "shift-dot"
+    ['"'] = "shift-apostrophe" # 注意雙引號需要跳脫
+    [":"] = "shift-semicolon"
+    ["|"] = "shift-backslash"
+    ["~"] = "shift-grave_accent"
+    ["{"] = "shift-bracket_left"
+    ["}"] = "shift-bracket_right"
+    ["!"] = "shift-1"
+    ["@"] = "shift-2"
+    ["#"] = "shift-3"
+    ["$"] = "shift-4"
+    ["%"] = "shift-5"
+    ["^"] = "shift-6"
+    ["&"] = "shift-7"
+    ["*"] = "shift-8"
+    ["("] = "shift-9"
+    [")"] = "shift-0"
   )
-  send_string=""
+
   for ((i = 0; i < ${#1}; i++)); do
     character=${1:i:1}
-    if [[ -n "${keymap[$character]}" ]]; then
-      send_string+="${keymap[$character]} "
+
+    if [[ "$character" =~ [a-z] ]]; then  # 判斷是否為小寫字母
+      key_name="$character"
+    elif [[ "$character" =~ [A-Z] ]]; then  # 判斷是否為大寫字母
+      key_name="shift-${character,,}"  # 轉換為小寫並加上 shift- 前綴
+    elif [[ -v char_map["$character"] ]]; then # 檢查是否在關聯陣列中
+      key_name="${char_map["$character"]}"
     else
-      send_string+="$character "
+      key_name="$character" # 預設情況，直接使用字元本身 (例如數字)
+      # key_name="" # 或者，如果遇到未定義字元，可以設定為空字串或跳過
+      # continue # 或者，跳過未定義字元
+    fi
+
+    if [[ -n "$key_name" ]]; then # 確保 key_name 不是空字串才發送
+      qm sendkey $VMID "$key_name"
     fi
   done
-  qm sendkey $VMID "$send_string"
   qm sendkey $VMID ret
 }
 
