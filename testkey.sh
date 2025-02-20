980b533088d202ec03a7e25a8da723ce89903f9d
plusisland <plusisland@gmail.com>
2025-02-20 16:31:06 +0800

    Create testkey.sh

---
+++ testkey.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+# 這個函數會根據QEMU的鍵盤編碼將文字轉換為sendkey命令
+send_keys() {
+    local vmid="$1"    # 虛擬機ID
+    local text="$2"     # 要轉換的文字
+
+    # 創建一個鍵位對應表，去掉了小寫字母和數字
+    declare -A key_map=(
+        ['A']='shift+a'
+        ['B']='shift+b'
+        ['C']='shift+c'
+        ['D']='shift+d'
+        ['E']='shift+e'
+        ['F']='shift+f'
+        ['G']='shift+g'
+        ['H']='shift+h'
+        ['I']='shift+i'
+        ['J']='shift+j'
+        ['K']='shift+k'
+        ['L']='shift+l'
+        ['M']='shift+m'
+        ['N']='shift+n'
+        ['O']='shift+o'
+        ['P']='shift+p'
+        ['Q']='shift+q'
+        ['R']='shift+r'
+        ['S']='shift+s'
+        ['T']='shift+t'
+        ['U']='shift+u'
+        ['V']='shift+v'
+        ['W']='shift+w'
+        ['X']='shift+x'
+        ['Y']='shift+y'
+        ['Z']='shift+z'
+        ['\n']='enter'
+        [' ']='space'
+        ['\t']='tab'
+        [',']='comma'
+        ['.']='period'
+        ['!']='shift+1'
+        ['@']='shift+2'
+        ['#']='shift+3'
+        ['$']='shift+4'
+        ['%']='shift+5'
+        ['^']='shift+6'
+        ['&']='shift+7'
+        ['*']='shift+8'
+        ['(']='shift+9'
+        [')']='shift+0'
+        ['-']='minus'
+        ['_']='shift+minus'
+        ['=']='equal'
+        ['+']='shift+equal'
+        ['[']='leftbracket'
+        [']']='rightbracket'
+        ['{']='shift+leftbracket'
+        ['}']='shift+rightbracket'
+        ['\\']='backslash'
+        ['|']='shift+backslash'
+        [';']='semicolon'
+        [':']='shift+semicolon'
+        ["'"]='apostrophe'
+        ['"']='shift+apostrophe'
+        ["/"]='slash'
+        ['?']='shift+slash'
+        ['`']='backtick'
+        ['~']='shift+backtick'
+        ['<']='comma'
+        ['>']='period'
+    )
+
+    # 遍歷輸入文字，並發送對應的sendkey命令
+    for (( i=0; i<${#text}; i++ )); do
+        char="${text:$i:1}"
+        
+        # 如果是小寫字母或數字，直接發送對應按鍵
+        if [[ "$char" =~ [a-z0-9] ]]; then
+            qm sendkey "$vmid" "$char"
+        elif [[ -v "key_map[$char]" ]]; then
+            key="${key_map[$char]}"
+            qm sendkey "$vmid" "$key"
+        else
+            echo "未找到對應的鍵: $char"
+        fi
+    done
+}
+
+# 用法示例
+send_keys 100 "Hello World!@.[]'123"
