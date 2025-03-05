a62ee7c6b8fe59e229d22e736739726094d398a2
plusisland <plusisland@gmail.com>
2025-03-05 17:18:26 +0800

    Update disable_high_availability.sh

--- disable_high_availability.sh
+++ disable_high_availability.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # https://free-pmx.pages.dev/guides/ha-disable
 
-# 永久停用 Proxmox High Availability (HA) 服務 | Permanently Disable Proxmox High Availability (HA) Service
+# 永久停用 Proxmox High Availability (HA) 服務 | Disable Proxmox High Availability (HA) Service Permanently.
 # 此腳本需在 Proxmox 集群的所有節點上執行。 | This script needs to be executed on all nodes in the Proxmox cluster.
 # 停用 HA 服務代表高可用性虛擬機器將不再於主機故障時自動移轉至其他節點，可能導致停機。 | Disabling HA service means that high-availability virtual machines will no longer automatically failover to another node in case of host failure, which may cause downtime.
 # 請務必了解後果，並確認不再需要 HA 功能後再執行。 | Please be sure to understand the consequences and confirm that you no longer need the HA function before executing.
