3182073bedf04d81b5b18f577b19f97632a053ff
plusisland <plusisland@gmail.com>
2025-03-29 16:56:07 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -191,7 +191,7 @@ send \"\r\"
 expect \"# \"
 send \"ln -s /sbin/poweroff /sbin/shutdown\r\"
 expect \"# \"
-if { $DRIVER_FIREWARE != "" } {
+if {![string equal \"$DRIVER_FIREWARE\" \"\"]} {
 send \"opkg install $DRIVER_FIREWARE wpad-openssl kmod-usb2-pci bluez-daemon\r\"
 expect \"Configuring bluez-daemon.\"
 sleep 5
